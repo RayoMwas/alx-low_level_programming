174bea733eb63429260f6e3811172e8c3a8a5438
RayoMwas <ryanmwangi61@gmail.com>
2023-07-13 13:18:08 -0700

    Print a simple statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Prints hello world 
+ *
+ *Return: 0 to print the statement
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
