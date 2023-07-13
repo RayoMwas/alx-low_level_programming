b12b88093a2447dbc66e7c4070ffd0ab2051c390
RayoMwas <ryanmwangi61@gmail.com>
2023-07-13 13:57:20 -0700

    Print an artistic statement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -prints the statement
+ *
+ * Return: 0 to print
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
