dad39263a1f8aaaec49435d1374f804e572b41f5
RayoMwas <ryanmwangi61@gmail.com>
2023-07-15 10:23:22 -0700

    Print all possible combinations of no.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- prints all possible combinations of no.
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
