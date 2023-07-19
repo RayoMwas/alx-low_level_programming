a005873af4838a0fa103a4b6a667c82f7a65bef4
RayoMwas <ryanmwangi61@gmail.com>
2023-07-19 02:52:50 -0700

    Check for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/**
+ * _islower- checks for lowercase alphabet
+ * @c: takes in a character
+ *
+ * Return: 1 if result if lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
