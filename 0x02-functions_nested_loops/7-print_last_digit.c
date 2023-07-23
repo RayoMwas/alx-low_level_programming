c01ec03aa8910228997652b901b403bb798ee705
RayoMwas <ryanmwangi61@gmail.com>
2023-07-23 13:26:08 -0700

    Print the value of last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+/**
+ * print_last_digit - prints the value of last digit
+ * @num: stores the last digit
+ *
+ * Return: prints the last digit
+ */
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
