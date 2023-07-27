378d5bdfa1c4bc5054f629ca468bdac67ca820cb
RayoMwas <ryanmwangi61@gmail.com>
2023-07-27 03:09:26 -0700

    Check for intetgers 0 through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+ * _isdigit - checks for numbers from 0 to 9
+ * @c: takes an integeter input
+ *
+ * Return: 1 if is within the range else return 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
