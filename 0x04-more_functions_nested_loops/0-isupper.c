6ea865295aabce88e433dfd54239223b0ba4a802
RayoMwas <ryanmwangi61@gmail.com>
2023-07-27 02:54:46 -0700

    Checks for lowercase characters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/**
+ * _isupper - checks for uppercase characters
+ * @c: Takes a charcater input
+ *
+ * Return: 1 if uppercse else return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
