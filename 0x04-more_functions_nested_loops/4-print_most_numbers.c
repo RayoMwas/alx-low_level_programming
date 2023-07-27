538325cb954ad06d9ebe4eb702f6192f4f4cd741
RayoMwas <ryanmwangi61@gmail.com>
2023-07-27 03:56:48 -0700

    Prints all integers except from 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+/**
+ * _putchar - prints single charactes
+ * @c: single character
+ *
+ * Return: 0
+ */
+int _putchar(char c);
+/**
+ * print_most_numbers - prints no 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
