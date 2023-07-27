d39fbb487f02a9f7aa9fc852a7e670895eb22a3b
RayoMwas <ryanmwangi61@gmail.com>
2023-07-27 03:15:58 -0700

    Multiply two integers and print the result

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * mul - multiplies two integers
+ * @a: takes the 1st integer
+ * @b: takes the 2nd integer
+ *
+ * Return: the result of multiplying
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
