cc6f2928b9e82acdbca4f74c757bdf14b1143a76
RayoMwas <ryanmwangi61@gmail.com>
2023-07-28 01:20:13 -0700

    Print the result of adding 2 no.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - adds 2 no. and retuns the result
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: sum of the 2 ints
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
