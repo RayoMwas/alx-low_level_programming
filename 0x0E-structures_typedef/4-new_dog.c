9dfcf5aead9bc1735c11bcdbd1b38618b4fdf519
Ryan Mwangi <ryanmwangi61@gmail.com>
2023-08-23 12:44:49 +0000

    Create a new struct dog containing multiple elements

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+/**
+ * new_dog - Creates a struct dog
+ * @name: character pointer that stores name of dog
+ * @age: integer pointer storing age of dog
+ * @owner: character pointer storing name of owner
+ *
+ * Return: new_dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->name = malloc(strlen(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+	new_dog->age = age;
+	new_dog->owner = malloc(strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->owner, owner);
+	return (new_dog);
+}
